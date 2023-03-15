3b9286cbbd5d543f22656b4947f8aa81c6285e83
Romenig Lima Damasio <romenigld@gmail.com>
2023-03-15 14:14:51 +0100

    formated

--- test/rld_live_view_studio_web/controllers/error_html_test.exs
+++ test/rld_live_view_studio_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule RldLiveViewStudioWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(RldLiveViewStudioWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(RldLiveViewStudioWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
--- test/rld_live_view_studio_web/controllers/error_json_test.exs
+++ test/rld_live_view_studio_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule RldLiveViewStudioWeb.ErrorJSONTest do
   use RldLiveViewStudioWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert RldLiveViewStudioWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert RldLiveViewStudioWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
