20cdc3e3f5f3e26c7f63a01bd4ed97d6e0a8b5a5
Romenig Lima Damasio <romenigld@gmail.com>
2023-03-21 17:40:47 +0100

    Chapter 7 - Search(Vehicles) - Notes & Exercises - Exercise: Find a Vehicle

--- lib/rld_live_view_studio_web/router.ex
+++ lib/rld_live_view_studio_web/router.ex
@@ -24,6 +24,7 @@ defmodule RldLiveViewStudioWeb.Router do
     live("/sales", SalesLive)
     live("/bingo", BingoLive)
     live("/flights", FlightsLive)
+    live("/vehicles", VehiclesLive)
   end
 
   # Other scopes may use custom stacks.
