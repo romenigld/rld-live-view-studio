208a2ab8c80437b573279a83369701bd21ed27d7
Romenig Lima Damasio <romenigld@gmail.com>
2023-03-22 18:55:45 +0100

    Chapter 8 - Autocomplete(Flights) - Notes & Exercises - Search As You Type

--- lib/rld_live_view_studio_web/live/flights_live.ex
+++ lib/rld_live_view_studio_web/live/flights_live.ex
@@ -19,7 +19,7 @@ defmodule RldLiveViewStudioWeb.FlightsLive do
     ~H"""
     <h1>Find a Flight</h1>
     <div id="flights">
-      <form phx-submit="search" phx-change="suggest">
+      <form phx-change="search">
         <input
           type="text"
           name="airport"
