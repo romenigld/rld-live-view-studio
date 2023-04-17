c9c8c7b52bb105645115039cd154e4dc746954f7
Romenig Lima Damasio <romenigld@gmail.com>
2023-04-17 19:15:41 +0200

    12 - Sorting(PizzaOrders) - Exercise: Sort Pizza Orders - 'pizza_orders' sorted by ':size' with the <.link>

--- lib/rld_live_view_studio_web/live/pizza_orders_live.ex
+++ lib/rld_live_view_studio_web/live/pizza_orders_live.ex
@@ -5,11 +5,27 @@ defmodule RldLiveViewStudioWeb.PizzaOrdersLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
-    socket =
-      assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
-      )
-
     {:ok, socket}
   end
+
+  def handle_params(params, _uri, socket) do
+    sort_order = (params["sort_order"] || "asc") |> String.to_existing_atom()
+    sort_by = (params["sort_by"] || "id") |> String.to_existing_atom()
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    socket =
+      assign(socket,
+        pizza_orders: PizzaOrders.list_pizza_orders(options),
+        options: options
+      )
+
+    {:noreply, socket}
+  end
+
+  defp next_sort_order(:asc), do: :desc
+  defp next_sort_order(:desc), do: :asc
 end
