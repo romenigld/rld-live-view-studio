8bec2719c55592ea2a524a10baa97455c2b6c638
Romenig Lima Damasio <romenigld@gmail.com>
2023-04-18 17:42:33 +0200

    12 - Sorting(PizzaOrders) - Exercise: Sort Pizza Orders - sort_indicator for the column of the sort order

--- lib/rld_live_view_studio_web/live/pizza_orders_live.ex
+++ lib/rld_live_view_studio_web/live/pizza_orders_live.ex
@@ -31,13 +31,12 @@ defmodule RldLiveViewStudioWeb.PizzaOrdersLive do
   slot(:inner_block, required: true)
 
   def sort_link(assigns) do
-    IO.inspect(assigns, label: "ASSIGNS")
-
     ~H"""
     <.link patch={
       ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: next_sort_order(@options.sort_order)}}"
     }>
       <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
     </.link>
     """
   end
@@ -65,4 +64,14 @@ defmodule RldLiveViewStudioWeb.PizzaOrdersLive do
   end
 
   defp valid_sort_by(_params), do: :id
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
 end
