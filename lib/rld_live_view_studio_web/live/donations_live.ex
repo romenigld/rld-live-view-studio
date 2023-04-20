909deab23588891e0f11374d9a7aea0c262825fb
Romenig Lima Damasio <romenigld@gmail.com>
2023-04-20 18:20:33 +0200

    13 - Pagination(Donations) - Notes & Exercises - Exercise: Validate User-Supplied Parameters

--- lib/rld_live_view_studio_web/live/donations_live.ex
+++ lib/rld_live_view_studio_web/live/donations_live.ex
@@ -18,8 +18,8 @@ defmodule RldLiveViewStudioWeb.DonationsLive do
     sort_by = valid_sort_by(params)
     sort_order = valid_sort_order(params)
 
-    page = (params["page"] || "1") |> String.to_integer()
-    per_page = (params["per_page"] || "5") |> String.to_integer()
+    page = param_to_integer(params["page"], 1)
+    per_page = param_to_integer(params["per_page"], 5)
 
     options = %{
       sort_by: sort_by,
@@ -97,4 +97,13 @@ defmodule RldLiveViewStudioWeb.DonationsLive do
   end
 
   defp valid_sort_order(_params), do: :asc
+
+  defp param_to_integer(nil, default), do: default
+
+  defp param_to_integer(param, default) do
+    case Integer.parse(param) do
+      {number, _} -> number
+      :error -> default
+    end
+  end
 end
