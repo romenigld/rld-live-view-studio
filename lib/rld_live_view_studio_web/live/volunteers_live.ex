9de5898f9a51b0f113caaf0c77c643a9b970a357
Romenig Lima Damasio <romenigld@gmail.com>
2023-05-02 20:16:44 +0200

    14 - Live Ecto Forms and Lists (2) - fixed the errors message

--- lib/rld_live_view_studio_web/live/volunteers_live.ex
+++ lib/rld_live_view_studio_web/live/volunteers_live.ex
@@ -21,13 +21,14 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form}>
+      <.form for={@form} phx-submit="save">
         <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
         <.input field={@form[:phone]} type="tel" placeholder="Phone" autocomplete="off" />
-        <.button>
+        <.button phx-disable-with="Saving...">
           Check In
         </.button>
       </.form>
+
       <pre>
         <%#= inspect(@form, pretty: true) %>
       </pre>
@@ -47,5 +48,24 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        {:noreply, assign(socket, :form, to_form(changeset))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
   end
 end
