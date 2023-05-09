045239c515dea2b3cfec20e5bbc684527571c02e
Romenig Lima Damasio <romenigld@gmail.com>
2023-05-09 19:50:12 +0200

    17 - Toggling State - Notes & Exercises - Exercise: Delete Volunteers

--- lib/rld_live_view_studio_web/live/volunteers_live.ex
+++ lib/rld_live_view_studio_web/live/volunteers_live.ex
@@ -58,10 +58,29 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
                 else: "Check Out" %>
             </button>
           </div>
+          <.link
+            class="delete"
+            phx-click="delete"
+            phx-value-id={volunteer.id}
+            data-confirm="Are you sure?"
+          >
+            <.icon name="hero-trash-solid" />
+          </.link>
         </div>
       </div>
     </div>
     """
+  end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    volunteer = Volunteers.get_volunteer!(id)
+    {:ok, _} = Volunteers.delete_volunteer(volunteer)
+
+    socket = stream_delete(socket, :volunteers, volunteer)
+
+    IO.inspect(socket.assigns.streams.volunteers, label: "delete")
+
+    {:noreply, socket}
   end
 
   def handle_event("toggle-status", %{"id" => id}, socket) do
