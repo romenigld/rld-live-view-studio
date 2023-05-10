bf4f8d80d7d0263906f58598f8ce8e13d4ff5adf
Romenig Lima Damasio <romenigld@gmail.com>
2023-05-10 19:27:42 +0200

    18 - Live Components(1)

---
+++ lib/rld_live_view_studio_web/live/volunteer_form_component.ex
@@ -0,0 +1,57 @@
+defmodule RldLiveViewStudioWeb.VolunteerFormComponent do
+  use RldLiveViewStudioWeb, :live_component
+  alias RldLiveViewStudio.Volunteers
+  alias RldLiveViewStudio.Volunteers.Volunteer
+
+  def mount(socket) do
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
+    {:ok, assign(socket, :form, to_form(changeset))}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form for={@form} phx-submit="save" phx-change="validate" phx-target={@myself}>
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" phx-debounce="2000" />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          autocomplete="off"
+          phx-debounce="blur"
+        />
+        <.button phx-disable-with="Saving...">
+          Check In
+        </.button>
+      </.form>
+    </div>
+    """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        send(self(), {:volunteer_created, volunteer})
+
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+
+        socket = put_flash(socket, :info, "Volunteer successfully checked in!")
+
+        {:noreply, assign(socket, :form, to_form(changeset))}
+
+      {:error, changeset} ->
+        socket = put_flash(socket, :error, "Volunteer form has an error!")
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
+  end
+
+  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
+    changeset =
+      %Volunteer{}
+      |> Volunteers.change_volunteer(volunteer_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+end
--- lib/rld_live_view_studio_web/live/volunteers_live.ex
+++ lib/rld_live_view_studio_web/live/volunteers_live.ex
@@ -2,18 +2,14 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
   use RldLiveViewStudioWeb, :live_view
   alias RldLiveViewStudio.Volunteers
   alias RldLiveViewStudio.Volunteers.Volunteer
+  alias RldLiveViewStudioWeb.VolunteerFormComponent
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
-    changeset = Volunteers.change_volunteer(%Volunteer{})
-
     socket =
       socket
       |> stream(:volunteers, volunteers)
-      |> assign(:form, to_form(changeset))
-
-    IO.inspect(socket.assigns.streams.volunteers, label: "mount")
 
     {:ok, socket}
   end
@@ -22,7 +18,7 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.volunteer_form form={@form} />
+      <.live_component module={VolunteerFormComponent} id={:new} />
 
       <pre>
         <%#= inspect(@form, pretty: true) %>
@@ -35,24 +31,6 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
         />
       </div>
     </div>
-    """
-  end
-
-  def volunteer_form(assigns) do
-    ~H"""
-    <.form for={@form} phx-submit="save" phx-change="validate">
-      <.input field={@form[:name]} placeholder="Name" autocomplete="off" phx-debounce="2000" />
-      <.input
-        field={@form[:phone]}
-        type="tel"
-        placeholder="Phone"
-        autocomplete="off"
-        phx-debounce="blur"
-      />
-      <.button phx-disable-with="Saving...">
-        Check In
-      </.button>
-    </.form>
     """
   end
 
@@ -90,8 +68,6 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
 
     socket = stream_delete(socket, :volunteers, volunteer)
 
-    IO.inspect(socket.assigns.streams.volunteers, label: "delete")
-
     {:noreply, socket}
   end
 
@@ -110,33 +86,7 @@ defmodule RldLiveViewStudioWeb.VolunteersLive do
     {:noreply, socket}
   end
 
-  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
-    case Volunteers.create_volunteer(volunteer_params) do
-      {:ok, volunteer} ->
-        socket = stream_insert(socket, :volunteers, volunteer, at: 0)
-
-        IO.inspect(socket.assigns.streams.volunteers, label: "save")
-
-        changeset = Volunteers.change_volunteer(%Volunteer{})
-
-        socket = put_flash(socket, :info, "Volunteer successfully checked in!")
-
-        {:noreply, assign(socket, :form, to_form(changeset))}
-
-      {:error, changeset} ->
-        socket = put_flash(socket, :error, "Volunteer form has an error!")
-        {:noreply, assign(socket, :form, to_form(changeset))}
-    end
-  end
-
-  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
-    IO.inspect(socket.assigns.streams.volunteers, label: "validate")
-
-    changeset =
-      %Volunteer{}
-      |> Volunteers.change_volunteer(volunteer_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, assign(socket, :form, to_form(changeset))}
+  def handle_info({:volunteer_created, volunteer}, socket) do
+    {:noreply, stream_insert(socket, :volunteers, volunteer, at: 0)}
   end
 end
