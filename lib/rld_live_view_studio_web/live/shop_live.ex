fff1e2bd1da01e5bf14dd563ef846b92aba4876a
Romenig Lima Damasio <romenigld@gmail.com>
2023-06-19 19:42:07 +0200

    JS Commands (1) - using the JS.show() and JS.hide()

--- lib/rld_live_view_studio_web/live/shop_live.ex
+++ lib/rld_live_view_studio_web/live/shop_live.ex
@@ -2,6 +2,7 @@ defmodule RldLiveViewStudioWeb.ShopLive do
   use RldLiveViewStudioWeb, :live_view
 
   alias RldLiveViewStudio.Products
+  alias Phoenix.LiveView.JS
 
   def mount(_params, _session, socket) do
     {:ok,
@@ -12,10 +13,11 @@ defmodule RldLiveViewStudioWeb.ShopLive do
      )}
   end
 
-  def handle_event("toggle-show-cart", _, socket) do
-    socket = update(socket, :show_cart, fn show -> !show end)
-    {:noreply, socket}
-  end
+  # Using the Phoenix.LiveView.JS for show and hide the cart, don't need anymore the handle event 'toggle-show-cart'
+  # def handle_event("toggle-show-cart", _, socket) do
+  #   socket = update(socket, :show_cart, fn show -> !show end)
+  #   {:noreply, socket}
+  # end
 
   def handle_event("add-product", %{"product" => product}, socket) do
     cart = Map.update(socket.assigns.cart, product, 1, &(&1 + 1))
